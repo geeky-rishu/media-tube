305a1e468a636040a8558f24fcb8e510cf57836f
Rishabh Sharma <87926819+geeky-rishu@users.noreply.github.com>
2026-01-20 00:20:58 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
-const GOOGLE_API_KEY = "AIzaSyC7Rl5BBLlBSjedCIfmW1TxOEgN05LXEz8";
+const GOOGLE_API_KEY = "";
 export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=20&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
 export const YOUTUBE_SEARCH_API = `https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
 
 // export const YOTUBE_SEARCH_API = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&key=${GOOGLE_API_KEY}&q=`;
+
